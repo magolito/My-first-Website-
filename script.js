0c96a5a0348a31f21abaac7bf156432efe56ff9f
Manuel Avila <magolitonft2311@gmail.com>
2023-03-31 12:47:45 -0400

    mouseover envent

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "My name is Manuel Avila, I am hoping to become a Software Engineer.";
 document.querySelector("body").appendChild(h2);
-
-const technicalSkills = document.getElementById('technical-skills');
-
-const skills = [
-  'JavaScript',
-  'HTML',
-  'CSS',
-  'And a lot of creativity'
-];
-
-const skillList = document.createElement('ul');
-
-skills.forEach(skill => {
-  const skillElement = document.createElement('li');
-  skillElement.textContent = skill;
-  skillList.appendChild(skillElement);
+const headingOne = document.querySelector('h1');
+headingOne.addEventListener('mouseover', () => {
+    headingOne.style.color = 'green';
+    headingOne.style.cursor = 'pointer';
 });
-
-technicalSkills.appendChild(skillList);
-
-
-
