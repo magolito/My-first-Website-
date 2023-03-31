2ff0268ab21ec013b28f498ed3a4319a547917d3
Manuel Avila <magolitonft2311@gmail.com>
2023-03-31 12:24:03 -0400

    my technical skills section

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "My name is Manuel Avila, I am hoping to become a Software Engineer.";
 document.querySelector("body").appendChild(h2);
+
+const technicalSkills = document.getElementById('technical-skills');
+
+const skills = [
+  'JavaScript',
+  'HTML',
+  'CSS',
+  'And a lot of creativity'
+];
+
+const skillList = document.createElement('ul');
+
+skills.forEach(skill => {
+  const skillElement = document.createElement('li');
+  skillElement.textContent = skill;
+  skillList.appendChild(skillElement);
+});
+
+technicalSkills.appendChild(skillList);
+
+
+
